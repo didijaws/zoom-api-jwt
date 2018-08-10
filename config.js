b69b14d9d113462194b5997bc0f0f2c9ae36e8f5
didijaws <42219914+didijaws@users.noreply.github.com>
2018-08-10 15:14:22 -0300

    teste
    teste

--- config.js
+++ config.js
@@ -3,8 +3,8 @@ const env = process.env.NODE_ENV || 'development'
 //insert your API Key & Secret for each environment, keep this file local and never push it to repo.
 const config = {
 	development :{
-		APIKey : '',
-		APISecret : ''
+		APIKey : 'SC_b7z4aRL2Yl9GkWCmh3g',
+		APISecret : 'pVx2EklOtt82Sofz0jroMDRz5ZvOSe4SjkHe'
 	},
 	production:{	
 		APIKey : '',
